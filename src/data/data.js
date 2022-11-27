9fedcb4ffc7c9fe4c73c279ee057c33a6d806e80
ArianDervishaj <ArianDervishaj@icloud.com>
2022-11-27 01:36:20 +0100

    preparation de la base de donnée

---
+++ src/data/data.js
@@ -0,0 +1,43 @@
+export default [
+    {
+        title: 'Stresa',
+        mapLink: 'https://goo.gl/maps/a7Krv2nRvRs6zsGF9',
+        startDate: '26 Aug 2022',
+        endDate: '28 Aug 2022',
+        description: "Stresa is a resort town on Italy's Lake Maggiore. Landmark buildings include Villa Pallavicino, with its gardens and zoo, and the Grand Hôtel des Îles Borromées, where author Hemingway once stayed.",
+        image: "https://unsplash.com/photos/qtWOrn6aGwc"
+    },
+    {
+        title: 'Cinque Terre',
+        mapLink: 'https://goo.gl/maps/4Pj4ukZTAtLAnPRZ8',
+        startDate: '22 Aug 2022',
+        endDate: '26 Aug 2022',
+        description: "Cinque Terre is a string of centuries-old seaside villages on the rugged Italian Riviera coastline. In each of the 5 towns, colorful houses and vineyards cling to steep terraces, harbors are filled with fishing boats and trattorias turn out seafood specialties along with the Liguria region's famous sauce, pesto",
+        image: "https://unsplash.com/photos/rknrvCrfS1k"
+    },
+    {
+        title: 'Milano',
+        mapLink: 'https://goo.gl/maps/mN238voDKxy5FDEPA',
+        startDate: '21 Aug 2022',
+        endDate: '22 Aug 2022',
+        description: "Milan, a metropolis in Italy's northern Lombardy region, is a global capital of fashion and design. Home to the national stock exchange, it's a financial hub also known for its high-end restaurants and shops. The Gothic Duomo di Milano cathedral and the Santa Maria delle Grazie convent, housing Leonardo da Vinci's mural “The Last Supper,” testify to centuries of art and culture.",
+        image: "https://unsplash.com/photos/bODFQ7D3JYo"
+    },
+    {
+        title: 'Vercorin',
+        mapLink: 'https://goo.gl/maps/EtxZ6dX4B5rEDQfH9',
+        startDate: '14 Feb 2022',
+        endDate: '18 Feb 2022',
+        description: 'Vercorin is a village in the Swiss Alps, located in the canton of Valais. The village is situated in the central part of the canton near Sierre, at a height of 1,320 metres, on a plateau overlooking the Rhone valley.',
+        image: "https://unsplash.com/photos/ybk1Epw_PoE"
+    },
+    {
+        title: 'Neuchâtel',
+        mapLink: 'https://goo.gl/maps/iEv5rXYd3UT7vicy9',
+        startDate: '31 Oct 2021',
+        endDate: '31 Oct 2021',
+        description: 'The city of Neuchâtel, the French-speaking capital of the Swiss canton Neuchâtel, lies on the northern shore of Lake Neuchâtel.',
+        image: "https://unsplash.com/photos/JZiMQY-zSp4"
+    },
+
+]
